24516ee221aade89ac33144dc08cc947813e6762
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-07 19:35:59 -1000

    han solo

--- js/app.js
+++ js/app.js
@@ -1,23 +1,32 @@
-let person4Hame = document.getElementById('person4Name');
+let person4Name = document.getElementById('person4Name');
 let person4Homeworld = document.getElementById('person4HomeWorld');
+let person14Name = document.getElementById('person14Name');
+let person14Species = document.getElementById('person14Species');
 
 var oReq = new XMLHttpRequest();
-function getName () {
-  personName = JSON.parse(this.responseText);
-  person4Name.innerHTML = personName.name;
+function getName4 () {
+  name4 = JSON.parse(this.responseText);
+  person4Name.innerHTML = name4.name;
 }
-oReq.addEventListener('load', getName);
+oReq.addEventListener('load', getName4);
 oReq.open('GET', 'http://swapi.co/api/people/4/', true);
 oReq.send();
 
 var oReq = new XMLHttpRequest();
-function getHomeworld () {
+function getHomeworld4 () {
   homeworld = JSON.parse(this.responseText);
-  console.log(homeworld);
-  console.log(person4Homeworld);
   person4Homeworld.innerHTML = homeworld.name;
 }
-oReq.addEventListener('load', getHomeworld);
+oReq.addEventListener('load', getHomeworld4);
 oReq.open('GET', 'http://swapi.co/api/planets/1/', true);
+oReq.send();
+
+var oReq = new XMLHttpRequest();
+function getName14 () {
+  name14 = JSON.parse(this.responseText);
+  person14Name.innerHTML = name14.name;
+}
+oReq.addEventListener('load', getName14);
+oReq.open('GET', 'http://swapi.co/api/people/14/', true);
 oReq.send();
 
