e179d60b8caf799ca18412ee5ab9b369533ddf7a
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-25 19:19:49 -1000

    han solo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,43 @@
+/* jshint esversion:6*/
 const person4Name = document.getElementById('person4Name');
 const person4Homeworld = document.getElementById('person4HomeWorld');
 const person14Name = document.getElementById('person14Name');
 const person14Species = document.getElementById('person14Species');
 const filmList = document.getElementById('filmList');
 
-var oReq = new XMLHttpRequest();
+
+
+fetch('http://swapi.co/api/people/4/').then((response) => {
+  return response.json();
+}).then((response) => {
+  person4Name.innerHTML = response.name;
+  return response.homeworld;
+}).then((response) => {
+  return fetch (response);
+}).then((response) => {
+  return response.json();
+}).then((response) => {
+  person4Homeworld.innerHTML = response.name;
+});
+
+fetch('http://swapi.co/api/people/14/').then((response) => {
+  return response.json();
+}).then((response) => {
+  console.log(response);
+  person14Name.innerHTML = response.name;
+  return fetch(response.species);
+}).then((response) => {
+  return response.json();
+}).then((response) => {
+  person14Species.innerHTML = response.name;
+});
+
+
+
+
+
+
+/*var oReq = new XMLHttpRequest();
 function getName4 () {
   name4 = JSON.parse(this.responseText);
   person4Name.innerHTML = name4.name;
@@ -59,8 +92,8 @@ function getFilms () {
 
       let planetLi = document.createElement('li');
       planetUl.appendChild(planetLi);
-      var oReq = new XMLHttpRequest();
 
+      var oReq = new XMLHttpRequest();
       function getFilmPlanets () {
         filmPlanets = JSON.parse(this.responseText);
         planetLi.innerHTML = filmPlanets.name;
@@ -73,4 +106,4 @@ function getFilms () {
 }
 oReq.addEventListener('load', getFilms);
 oReq.open('GET', 'http://swapi.co/api/films/', true);
-oReq.send();
+oReq.send();*/
